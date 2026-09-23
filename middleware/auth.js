904aefebfeb26f93b446e1ca8ea834866ccd97d8
Carlos <sa475677@uaeh.edu.mx>
2026-09-22 20:04:51 -0600

    Correcion de sentencia mal escrita

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,4 +1,4 @@
-const { createClient } = requiere('@supabase/supabase-js');
+const { createClient } = require('@supabase/supabase-js');
 
 const supabaseAdmin = createClient(
     process.env.SUPABASE_URL,
